19ff54a60ce4e8727e9aad1c6a670bc9901a6087
Colin <colindcirian@gmail.com>
2023-07-29 11:12:07 -0700

    added authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -3,22 +3,45 @@ const db = require("../models");
 //const bcrypt = require('bcrypt')
 
 const { User } = db;
-
-  router.post("/", async (req, res) => {
+  
+router.post('/', async (req, res) => {
+    
     let user = await User.findOne({
-      where: { email: req.body.email },
-    });
+        where: { email: req.body.email }
+    })
 
-    if (
-      !user ||
-      !(await bcrypt.compare(req.body.password, user.passwordDigest))
-    ) {
-      res.status(404).json({
-        message: `Could not find a user with the provided username and password`,
-      });
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
     } else {
-      res.json({ user });
+        req.session.userId = user.userId
+        res.json({ user })                                       
     }
-  });
+})
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId                    
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId){
+        console.log('Do the really super important thing')
+        res.send('Done')
+    } else {
+        console.log('You are not authorized to do the super important thing')
+        res.send('Denied')
+    }
+})
 
 module.exports = router;
